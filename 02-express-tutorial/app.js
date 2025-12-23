ed2af736acbae8c1892bfa7b50724158ff98d663
NatalyBMota <interactivedesign@yahoo.com>
2025-12-22 19:23:59 -0500

    Created a get request to find data about a person based on id, using the /api/v1/people/:id route, as well as a put request to update data about a person (such as his/her name) based on an id number, sing the same route. Also created a delete request to remove a user (or person) from the array based on ID number. Did so by following along with the Node course on video from FreeCodeCamp, as well as by roughly following along with the instructions for CTD's Middleware REST Methods and Postman assigmment. Only, thus far, I used app.get, app.put, and app.delete, as opposed to using a router. Yet, I plan on refactoring this code slightly, to use a router, as well as to finish meeting assignment requirements.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -23,6 +23,47 @@ app.get('/api/v1/people', (req, res) => {
     res.status(200).json({success: true, data:people})
 })
 
+app.get('/api/v1/people/:id', (req, res) => {
+    const {id} = req.params
+    const person = people.find((person) => person.id === Number(id))
+    if (!person) {
+        return res.status(404).json({success: false, msg: `no person with id ${id}`})
+    } else {
+        return res.status(200).json({success: true, data: person})
+    }
+})
+
+app.put('/api/v1/people/:id', (req, res) => {
+    const {id} = req.params
+    const {name} = req.body
+    // console.log(id, name)
+    // res.send('hello world')
+    const person = people.find((person) => person.id === Number(id))
+
+    if (!person) {
+        return res
+            .status(404)
+            .json({success: false, msg: `no person with id ${id}`})
+    }
+    const newPeople = people.map((person) => {
+        if (person.id === Number(id)) {
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json({success: true, data: newPeople})
+})
+
+app.delete('/api/v1/people/:id', (req, res) => {
+    const person = people.find((person) => person.id === Number(req.params.id))
+    if (!person) {
+        return res
+            .status(404)
+            .json({success: false, msg: `no person with id ${req.params.id}`})
+    }
+    const newPeople = people.filter((person) => person.id !== Number(req.params.id))
+    return res.status(200).json({success: true, data: newPeople})
+})
 
 app.post('/api/v1/people', (req, res) => {
     const name = req.body.name
