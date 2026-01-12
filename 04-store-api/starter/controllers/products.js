27e73e4040634c694b0bd776e67f7a76cbc7edc3
NatalyBMota <interactivedesign@yahoo.com>
2026-01-12 16:58:33 -0500

    Followed along with instructor provided code from the 'Node.js Projects' video-based course on YouTube, up until shortly before the 3:55:40 mark in the timeline.

---
+++ 04-store-api/starter/controllers/products.js
@@ -0,0 +1,19 @@
+const Product = require('../models/product')
+
+const getAllProductsStatic = async (req, res) => {
+    // throw new Error('testing async errors')
+    const products = await Product.find({ 
+        featured: true,  
+    })
+    // res.status(200).json({msg: 'products testing route'})
+    res.status(200).json({ products, nbHits: products.length })
+}
+
+const getAllProducts = async (req, res) => {
+    res.status(200).json({msg: 'products route'})
+}
+
+module.exports = {
+    getAllProducts,
+    getAllProductsStatic,
+}
