16027ecd7504d194912c11aa2661042d716e3cb9
NatalyBMota <interactivedesign@yahoo.com>
2026-01-12 17:53:35 -0500

    Followed along with instructor provided code up until a little before the 4:08:36 mark in the timeline.

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -3,14 +3,23 @@ const Product = require('../models/product')
 const getAllProductsStatic = async (req, res) => {
     // throw new Error('testing async errors')
     const products = await Product.find({ 
-        name: 'vase table',  
+        page: '2',  
     })
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
     // console.log(req.query)
-    const products = await Product.find(req.query)
+    const {featured} = req.query
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+
+    // const products = await Product.find(req.query)
+    console.log(queryObject)
+    const products = await Product.find(queryObject)
     // res.status(200).json({msg: 'products route'})
     res.status(200).json({ products, nbHits: products.length })
 }
