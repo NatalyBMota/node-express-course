6fa6b0eba91cecb82d22748c23b0f640e647b964
NatalyBMota <interactivedesign@yahoo.com>
2026-01-12 19:58:36 -0500

    Followed along with instructor provided code, and with his guided testing using Postman, up through the 4:36:27 mark in the timeline.

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -7,7 +7,8 @@ const getAllProductsStatic = async (req, res) => {
     /* const products = await Product.find({
         name: {$regex: search, $options: 'i'},
     }) */
-   const products = await Product.find({}).sort('-name price')
+    // const products = await Product.find({}).sort('-name price')
+    const products = await Product.find({}).select('name price')
     res.status(200).json({ products, nbHits: products.length })
 }
 
@@ -30,7 +31,11 @@ const getAllProducts = async (req, res) => {
     // console.log(queryObject)
     let result = Product.find(queryObject)
     if (sort) {
-        console.log(sort)
+        // console.log(sort)
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt')
     }
     const products = await result
     // res.status(200).json({msg: 'products route'})
