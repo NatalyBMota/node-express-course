a9bca619f5a480a57fc54af3736e0459896c3f86
NatalyBMota <interactivedesign@yahoo.com>
2026-01-12 20:45:48 -0500

    Followed along with instructor provide code, explanations, and guided testing using Postman and the browser, up until the 4:47:24 mark in the timeline.

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -8,11 +8,14 @@ const getAllProductsStatic = async (req, res) => {
         name: {$regex: search, $options: 'i'},
     }) */
     // const products = await Product.find({}).sort('-name price')
-    const products = await Product.find({})
+    /* const products = await Product.find({})
         .sort('name')
         .select('name price')
         .limit(10)
-        .skip(1)
+        .skip(5) */
+
+    const products = await Product.find({}).sort('name').select('name price')
+
     res.status(200).json({ products, nbHits: products.length })
 }
 
@@ -49,6 +52,15 @@ const getAllProducts = async (req, res) => {
         const fieldsList = fields.split(',').join(' ')
         result = result.select(fieldsList)
     }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+
+    result = result.skip(skip).limit(limit)
+    /* At the moment we have 23 products. So, if I decide to limit my response to only 7 items, how many pages do I have? Well, 23 / 7 is over 3, so we will have 4 pages. 7 * 3 is 21, so the last page will only have 2 items. 
+    If page is set to 1, we will skip 0 items. If page is set to 2 and limit is
+    set to 10, we will skip 10 items. If page is set to 3 and limit is set to 7, we will skip 14 items. */
 
     const products = await result
     // res.status(200).json({msg: 'products route'})
